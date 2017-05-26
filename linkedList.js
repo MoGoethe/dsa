00015dea62fcd4bb5e16ffd10f292e0f066c3f66
MoGoethe <1095364870@qq.com>
2017-05-27 07:11:27 +0800

    20170527

--- linkedList.js
+++ linkedList.js
@@ -5,19 +5,32 @@ function LinkedList(){
 	}
 	var length = 0;
 	var head =null;
-	this.append= function(item){
-
+	this.append= function(element){
+		//If linklist is null head = Node
+		var node = new Node(element),
+			current;
+		if(head === null){
+			head= node;
+		}else{
+			current = head; //form first node to lash node,if next node is existence node =node.next 
+			while(current.next){
+				current = current.next;  //get last node
+			}
+			current.next = node;
+		}
+		length ++;
+		this.print();
 	}
-	this.insert = function(item，position){
+	this.insert = function(element,position){
 
 	}
 	this.removeAt = function(position){
 
 	}
-	this.remove = function(item){
+	this.remove = function(element){
 
 	}
-	this.indexOf = function(item){
+	this.indexOf = function(element){
 
 	}
 	this.isEmpty = function(){
@@ -30,9 +43,32 @@ function LinkedList(){
 
 	}
 	this.toString = function(){
-
+		var linkListStr = '';
+		if(head===null){
+			return null	
+		}else{
+			var current = head;
+			while(current.next){
+				linkListStr = linkListStr + current.element + ',';
+				current = current.next;
+			}
+			linkListStr = linkListStr + current.element;
+			return linkListStr;
+		}
 	}
 	this.print = function(){
-		
+		var linkListPrintStr = this.toString();
+		linkListPrintStr === null ? console.log('linklist is nill') : console.log(linkListPrintStr);
 	}
 }
+var ll = new LinkedList();
+ll.append(5);
+ll.append(10);
+
+
+
+
+
+
+
+
