3f9f04775b56159503c05fb649a4689f248c1d0c
MoGoethe <1095364870@qq.com>
2017-05-26 23:15:35 +0800

    update

---
+++ linkedList.js
@@ -0,0 +1,38 @@
+function LinkedList(){
+	function Node(element) {
+		this.element = element;
+		this.next = null;
+	}
+	var length = 0;
+	var head =null;
+	this.append= function(item){
+
+	}
+	this.insert = function(item，position){
+
+	}
+	this.removeAt = function(position){
+
+	}
+	this.remove = function(item){
+
+	}
+	this.indexOf = function(item){
+
+	}
+	this.isEmpty = function(){
+		return length == 0;
+	}
+	this.size = function(){
+		return length;
+	}
+	this.getHead = function(){
+
+	}
+	this.toString = function(){
+
+	}
+	this.print = function(){
+		
+	}
+}
